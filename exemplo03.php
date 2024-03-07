8b057a44f2ea0ba0a5b25ff9da6728217981fa2c
samuelvmt <samu.7v@gmail.com>
2024-03-06 22:01:01 -0300

    exemplo03.php

---
+++ exemplo03.php
@@ -0,0 +1,14 @@
+<?php
+  $data = '06 de março de 2024';
+  $salario = 850.00;
+  $cargo = "Estagiário";
+  $idade = 21;
+  $resultado = true;
+
+  if ($resultado) {
+  	echo('Verdadeiro');
+  }
+
+  	echo("<br/>Arquivo criado em $data");
+
+?>
